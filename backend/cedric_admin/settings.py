bbdf068e7c3f5a217a6417b2de16eed19b658ffc
Cedric Munyai <cedric@munyai.com>
2025-12-28 11:58:31 +0200

    Fix S3 domain to include region for eu-north-1 bucket

--- backend/cedric_admin/settings.py
+++ backend/cedric_admin/settings.py
@@ -77,7 +77,13 @@ if config('USE_S3', default=False, cast=bool):
     AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY', default='')
     AWS_STORAGE_BUCKET_NAME = config('AWS_STORAGE_BUCKET_NAME', default='')
     AWS_S3_REGION_NAME = config('AWS_S3_REGION_NAME', default='us-east-1')
-    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+    
+    # S3 domain with region for non-us-east-1 regions
+    if AWS_S3_REGION_NAME == 'us-east-1':
+        AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+    else:
+        AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.{AWS_S3_REGION_NAME}.amazonaws.com'
+    
     AWS_LOCATION = 'media'
     AWS_QUERYSTRING_AUTH = False  # Make URLs public
     
